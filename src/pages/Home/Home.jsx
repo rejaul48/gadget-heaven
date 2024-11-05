a4950d3fe280e2785f91ce9993ec6dd83bfcd72b
rejaul25 <rejaulislammr25@gmail.com>
2024-11-05 20:31:00 +0600

    version 2.0.0.1

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
                         <h2 className='text-lg md:text-[36px] lg:text-[48px] xl:text-[56px] font-bold md:w-9/12 mx-auto leading-snug'>
                             Upgrade Your Tech Accessorize with Gadget Heaven Accessories
                         </h2>
-                        <p className='text-sm md:w-6/12 mx-auto my-3'>
+                        <p className='text-sm md:w-6/12 mx-auto my-3 px-3 xl:px-0'>
                             Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!
                         </p>
                         <Link to='/dashboard' className='bg-white py-2 px-8 rounded-full text-primary font-bold mt-2'>Shop Now</Link>
